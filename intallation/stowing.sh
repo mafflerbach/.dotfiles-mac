3b22c9f8491b4a0ae4e51def67afffc06e75a360
Maren Afflerbach <maren@afflerbach.info>
2021-08-09 10:56:55 +0200

    add fonts

---
+++ intallation/stowing.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+cd /home/maren/.dotfiles
+stow awesome
+stow compton
+stow goJira
+stow kitty
+stow misc
+stow qutebrowser
+stow rofi
+stow scripts
+stow tmux
+stow vim2
+
+rm ~/.zshrc
+stow zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+rm ~/.Xresources
+rm ~/.xinitrc
+stow xinit
+
+touch ~/exportedVars
+touch ~/.dotfiles/misc/.credencials
--- intallation/systemInstall.sh
+++ intallation/systemInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Suy --needed git base-devel zsh exa
-git clone https://aur.archlinux.org/yay.git
+it clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd /home/maren/.dotfiles/intallation
@@ -15,27 +15,8 @@ go get github.com/go-jira/jira/cmd/jira
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
 
-stow awesome
-stow compton
-stow goJira
-stow kitty
-stow misc
-stow qutebrowser
-stow rofi
-stow scripts
-stow tmux
-stow vim2
-
-rm ~/.zshrc
-stow zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-rm ~/.Xresources
-rm ~/.xinitrc
-stow xinit
-
-touch ~/exportedVars
-touch ~/.dotfiles/misc/.credencials
